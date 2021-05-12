6fd0ce9d9acc33ca15be8de73b59300f2f1930a2
sone <sthaya@gmail.com>
2021-05-11 20:18:12 -0700

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState(["movie 1", "movie 2"]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token b41fa5e9613329067b81bf277092e59820a1081a",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setMovies(resp))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="App">
