b91f4a9fbbcf17734ab57ca67af0754e87f13e59
sone <sthaya@gmail.com>
2021-05-13 20:55:04 -0700

    event and props

--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const MovieList = ({ movies }) => {
+  const movieClicked = (movie) => evt => {
+    props.movieClicked(movie)
+  }
+
   return (
     <div>
       {movies &&
         movies.map((movie) => {
-          return <h2>{movie.title}</h2>;
+          return (
+            <div key={movie.id}>
+              <h2 onClick={movieClicked(movie)}>{movie.title}</h2>
+            </div>
+          );
         })}
     </div>
   );
